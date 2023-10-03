fc46b52f6fe09b65350bf30afa94c7aec1199ef5
Amit Khatri <aamita96@gmail.com>
2023-10-03 22:45:58 +0530

    bubble-sort logic improved and examples added

--- Alogorithms/bubble-sort.js
+++ Alogorithms/bubble-sort.js
@@ -1,12 +1,18 @@
 // Input
-const arr = [8, 9, 5, 6458, 100, 12, 1];
-// Output
-[1, 5, 8, 9, 12, 100, 6458];
+const input1 = [8, 9, 5, 6458, 100, 12, 1];
+const input2 = [9, 5, 6, 1, 0];
+
+// Outputs
+
+// Output 1
+// [1, 5, 8, 9, 12, 100, 6458];
+// Output 2
+// [0,1,5,6,9]
 
 const bubbleSort = (arr) => {
     const n = arr.length;
     for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n - i; j++) {
+        for (let j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
             }
@@ -15,4 +21,4 @@ const bubbleSort = (arr) => {
     return arr;
 }
 
-console.log(bubbleSort(arr));
+console.log(bubbleSort(input2));
