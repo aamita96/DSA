23975076b4bafd35772d763f3723cb662943485d
Amit Khatri <aamita96@gmail.com>
2023-10-04 22:34:27 +0530

    bubble-sort logic improvised with comments

--- Alogorithms/bubble-sort.js
+++ Alogorithms/bubble-sort.js
@@ -13,6 +13,7 @@ const bubbleSort = (arr) => {
     const n = arr.length;
     for (let i = 0; i < n; i++) {
         for (let j = 0; j < n - i - 1; j++) {
+            // let j =0; j < n-1; j++ // This will do the job as well but above condition (n - i - 1) used is a bit optimized.
             if (arr[j] > arr[j + 1]) {
                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
             }
